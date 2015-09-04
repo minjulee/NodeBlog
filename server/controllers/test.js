7a4aa78d02226fa25be7a5cf7e16e9d9a0808449
abc <abc@mycom.me>
2015-09-04 19:01:40 +0900

    TypeScript 연동 Json => Typescript class 간 연동 실패

--- server/controllers/test.js
+++ server/controllers/test.js
@@ -2,15 +2,23 @@ var model = reqlib("/server/config/mysql.js");
 module.exports = {
     index: {
         handler : function(request, reply){
-
-            var helper = require("../config/dataHelper.ts");
             var test = require("../model/test.ts");
-
             model.GetSelect("select username, email, date_format(create_time, '%Y-%m-%d') create_time from user", function(recordset){
+                // 유저 리스트 클래스 초기화
+                var users = new test.Users();
 
-                var instance = helper.deserialize(recordset, test.Environment, test.Environment.user);
-                //console.log(instance);
+                for(var prop in recordset){
+                    if(typeof recordset[prop] === "object"){
+                        var user = new test.User();
+                        for(var propName in recordset[prop]){
+                            user[propName] = recordset[prop][propName];
+                        }
 
+                        users[prop] = user;
+                    }else{
+
+                    }
+                }
                 reply.view("test/index", {
                     title : "Email-Inbox",
                     data : recordset

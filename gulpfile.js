45a19e4b8c716a77940db9ae4c9c35be59795878
abc <abc@mycom.me>
2015-09-07 19:07:34 +0900

    #

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,9 @@
 var gulp = require('gulp');
-var webserver = require('gulp-webserver');
+var exec = require('child_process').exec;
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var minifyhtml = require('gulp-minify-html');
 var sass = require('gulp-sass');
-var livereload = require('gulp-livereload');
 
 var src = 'public/src';
 var dist = 'public/dist';
@@ -17,9 +16,12 @@ var paths = {
 };
 
 // 웹서버를 localhost:8000 로 실행한다.
-gulp.task('server', function () {
-    return gulp.src('/')
-        .pipe(webserver());
+gulp.task('server', function (cb) {
+    exec("node server.js", function(err, stdout, stderr){
+        console.log(stdout);
+        console.log(stderr);
+        cb(err);
+    });
 });
 
 // 자바스크립트 파일을 하나로 합치고 압축한다.
@@ -52,15 +54,6 @@ gulp.task('compress-html', function () {
 //        .pipe(gulp.dest(dist + '/css'));
 //});
 
-// 파일 변경 감지 및 브라우저 재시작
-gulp.task('watch', function () {
-    livereload.listen();
-    gulp.watch(paths.js, ['combine-js']);
-    //gulp.watch(paths.scss, ['compile-sass']);
-    gulp.watch(paths.css, ['combine-css']);
-    gulp.watch(paths.html, ['compress-html']);
-    gulp.watch(dist + '/**').on('change', livereload.changed);
-});
 
 //기본 task 설정
 gulp.task('default', [
@@ -68,6 +61,5 @@ gulp.task('default', [
     'combine-js',
     'combine-css',
     //'compile-sass',
-    'compress-html',
-    'watch'
+    'compress-html'
 ]);
